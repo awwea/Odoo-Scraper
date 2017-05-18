0ef3a28d9aa07a2adcb3659efa8d7f36ba3e306e
kazafy <kazafysayed@gmail.com>
2017-05-18 23:16:46 +0200

    scraper

--- model/myproduct.py
+++ model/myproduct.py
@@ -1,5 +1,5 @@
 from odoo import models, fields, api
-
+from ..util.scraper import Scrapper
 
 class MyProduct(models.Model):
     _name = 'scraper.product'
@@ -10,8 +10,15 @@ class MyProduct(models.Model):
     price = fields.Float(string='Price')
     description = fields.Char(string='Description')
     image = fields.Binary()
+    imagePath=''
     url = fields.Char(string='URL')
-    
 
 
+    def getData(self):
+        scraper = Scrapper(self.url)
+        self.name = scraper.getName()
+        self.description = scraper.getDescription()
+        # self.category = scraper.get_category()
+        self.price = scraper.getPrice()
+        self.imagePath = scraper.getImageUrl()
 
---
+++ util/scraper.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+class Scrapper:
+    def __init__(self, url):
+        self.source = requests.get(url)
+        self.soup = BeautifulSoup(self.source.content, "lxml")
+
+    def getName(self):
+        pn = self.soup.find("div", {"class": "small-12 columns product-title"})
+        product_name = pn.find("h1").text
+        return product_name
+
+    def getPrice(self):
+        price = self.soup.find("div", {"class": "text-default price-container"})
+        price_value = price.find("h3").text
+        return price_value.replace("EGP", "")
+
+    def getDescription(self):
+        description = self.soup.find("div", {"class": "item-details-mini"})
+        desc = description.find("p").text
+        return desc
+
+    def getImageUrl(self):
+        image = self.soup.find("div", {"class": "img-bucket"})
+        image_url = image.find('img').get('src')
+        return image_url
